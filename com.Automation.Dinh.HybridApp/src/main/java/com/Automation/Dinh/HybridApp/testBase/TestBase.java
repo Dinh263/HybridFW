541a9c00958e6dd756a91887588f6a6c77b874d7
Dinh263 <dinh.test263@hotmail.com>
2017-11-21 20:39:37 -0800

    take screen shot
    take screen shot

--- com.Automation.Dinh.HybridApp/src/main/java/com/Automation/Dinh/HybridApp/testBase/TestBase.java
+++ com.Automation.Dinh.HybridApp/src/main/java/com/Automation/Dinh/HybridApp/testBase/TestBase.java
@@ -4,11 +4,18 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+
 
 public class TestBase {
 	public WebDriver driver;
@@ -49,6 +56,20 @@ public class TestBase {
 		file= new File(System.getProperty("user.dir")+"/src/main/java/com/Automation/Dinh/HybridApp/config/or.properties");
 		inputStr = new FileInputStream(file);
 		OR.load(inputStr);
+	}
+	
+	public void getScreenShot(String imageName) throws IOException {
+		if(imageName.equals("")) {
+			imageName="_blank";
+		}
+		File fileScreenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		String imageLocation = System.getProperty("user.dir")+"/src/main/java/com/Automation/Dinh/HybridApp/screenshot";
+		Calendar calender = Calendar.getInstance();
+		SimpleDateFormat formater = new SimpleDateFormat("dd_MM_yyyy_hh_mm_ss");
+		String actualImageName = imageLocation+ imageName +"_"+formater.format(calender.getTime())+".png"; 
+		File destFile = new File(actualImageName);
+		FileUtils.copyDirectory(fileScreenshot, destFile);
+		
 	}
 	public void getPropertiesData() {
 		
