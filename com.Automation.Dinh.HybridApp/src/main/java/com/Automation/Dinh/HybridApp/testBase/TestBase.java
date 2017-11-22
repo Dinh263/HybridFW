d782a9a5c7c30a083fdd5e2ba6de58365214b1b5
Dinh263 <dinh.test263@hotmail.com>
2017-11-21 21:07:30 -0800

    wait for element
    wait for element

--- com.Automation.Dinh.HybridApp/src/main/java/com/Automation/Dinh/HybridApp/testBase/TestBase.java
+++ com.Automation.Dinh.HybridApp/src/main/java/com/Automation/Dinh/HybridApp/testBase/TestBase.java
@@ -12,8 +12,11 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 
@@ -56,6 +59,11 @@ public class TestBase {
 		file= new File(System.getProperty("user.dir")+"/src/main/java/com/Automation/Dinh/HybridApp/config/or.properties");
 		inputStr = new FileInputStream(file);
 		OR.load(inputStr);
+	}
+	
+	public void waitForElement(WebElement element) {
+		WebDriverWait wait = new WebDriverWait(driver, 120);
+		wait.until(ExpectedConditions.elementToBeClickable(element));
 	}
 	
 	public void getScreenShot(String imageName) throws IOException {
